be47e80b8d4dc7b75ae7aae8cfdce510ad6bb61b
Murphy Chan <53081144+aiyuta@users.noreply.github.com>
2024-04-15 07:11:11 +0800

    Update word_scraper.py
    fix error

--- word_scraper.py
+++ word_scraper.py
@@ -304,24 +304,24 @@ if search:
             page_img = requests.get(f"https://unsplash.com/s/photos/{keyword}")
             soup = BeautifulSoup(page_img.content, 'lxml')
             rows = soup.find_all("img", class_='ApbSI vkrMA')
-            
-            for i, row in enumerate(rows):
-                # Check if the row order is 1, 3, or 5
-                if (i + 1) % 2 != 0:
-                    list = row["src"]
-                    st.image(list)
+            try:
+                for i, row in enumerate(rows):
+                    # Check if the row order is 1, 3, or 5
+                    if (i + 1) % 2 != 0:
+                        list = row["src"]
+                        st.image(list)
             except:
                 pass
         with col2:        
             page_img = requests.get(f"https://unsplash.com/s/photos/{keyword}")
             soup = BeautifulSoup(page_img.content, 'lxml')
             rows = soup.find_all("img", class_='ApbSI vkrMA')
-            
-            for i, row in enumerate(rows):
-                # Check if the row order is 1, 3, or 5
-                if (i + 1) % 2 != 1:
-                    list = row["src"]
-                    st.image(list)
+            try:
+                for i, row in enumerate(rows):
+                    # Check if the row order is 1, 3, or 5
+                    if (i + 1) % 2 != 1:
+                        list = row["src"]
+                        st.image(list)
             except:
                 pass
 # random learning
